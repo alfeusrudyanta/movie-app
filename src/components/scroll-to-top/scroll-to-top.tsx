0c6ed8d43119f92c5526ba4433173953feb551b1
rudyanta.alfeus@gmail.com <rudyanta.alfeus@gmail.com>
2026-01-15 18:56:18 +0700

    scroll to top

--- src/components/scroll-to-top/scroll-to-top.tsx
+++ src/components/scroll-to-top/scroll-to-top.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export function ScrollToTop() {
+function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -10,3 +10,5 @@ export function ScrollToTop() {
 
   return null;
 }
+
+export { ScrollToTop };
