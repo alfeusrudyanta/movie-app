29fb720dd82e85f80ec72c4db9fe23ef74005a20
rudyanta.alfeus@gmail.com <rudyanta.alfeus@gmail.com>
2026-01-15 20:40:55 +0700

    update axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,9 @@
 import Axios from 'axios';
 
 const AxiosInstance = Axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   params: {
-    api_key: import.meta.env.VITE_REACT_APP_API_KEY,
+    api_key: import.meta.env.VITE_API_KEY,
   },
   headers: {
     'Content-Type': 'application/json',
